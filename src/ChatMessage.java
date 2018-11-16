10a230e2d9a3e245920d857cce3234a67d606b30
lee2968 <lee2968@purdue.edu>
2018-11-16 00:22:41 -0500

    Merge branch 'master' of C:\Users\user\IdeaProjects\Server and Client with conflicts.

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -7,9 +7,31 @@ final class ChatMessage implements Serializable {
     private static final long serialVersionUID = 6898543889087L;
     private String message;
     private int type;
+
     public ChatMessage(String message, int type) {
         this.message = message;
         this.type = type;
+    }
+
+    private synchronized void broadcast(String message) {
+
+    }
+
+    private boolean writeMessage(String msg) throws IOException {
+        return true;
+
+    }
+
+    private void remove(int id) {
+
+    }
+
+    public void run() {
+
+    }
+
+    private void close() {
+
     }
 
     public String getMessage() {
