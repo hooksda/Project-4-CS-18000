3ebf6728d4d2096ab278ca1e4bf0aa273d804bb7
lee2968 <lee2968@purdue.edu>
2018-11-21 02:56:33 -0500

    chicken butt

--- src/ChatFilter.java
+++ src/ChatFilter.java
@@ -8,17 +8,10 @@ public class ChatFilter {
             File file = new File(badWordsFileName);
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
+            FileWriter fw = new FileWriter(badWordsFileName);
             ArrayList<String> badwords = new ArrayList<>();
-            String word = "";
-            String asterisk = "*";
             for (String line; (line = br.readLine()) != null; ) {
-                String[] bad = line.split(" ");
-                for (int i = 0; i < bad.length; i++) {
-                    word = word + asterisk;
-                }
-                for (int i = 0; i < bad.length; i++) {
-                    badwords.add(bad[i]);
-                }
+                    badwords.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,6 +21,34 @@ public class ChatFilter {
     }
 
     public String filter(String msg) {
+        String s = "";
+        String x = "*";
+        boolean loop = false;
+        int lastWord = 0;
+        String word = "";
+        for (int j = 0; j < word.length(); j++) {
+            s = s + x;
+        }
+        while (!loop) {
+            int newWord = msg.indexOf(word, lastWord);
+            if (newWord == -1) {
+                loop = true;
+            } else {
+                lastWord = newWord + 1;
+                if ((newWord == 0 || !(Character.isLetter(msg.charAt(newWord - 1)))) &&
+                        (newWord + word.length() > msg.length() - 1 ||
+                                !(Character.isLetter(msg.charAt(newWord + word.length()))))) {
+                    String before = msg.substring(0, newWord);
+                    String after = "";
+                    if (newWord + word.length() > msg.length() - 1) {
+                        after = "";
+                    } else {
+                        after = msg.substring(newWord + word.length());
+                    }
+                    msg = before + s + after;
+                }
+            }
+        }
         return msg;
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -11,16 +11,20 @@ final class ChatServer {
     private static int uniqueId = 0;
     private final List<ClientThread> clients = new ArrayList<>();
     private final int port;
-
+    Date date = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    String chatTime = sdf.format(TimeZone.getTimeZone("America/Indiana/Indianapolis"));
 
     private ChatServer(int port) {
         this.port = port;
     }
+
     /*
      * This is what starts the ChatServer.
      * Right now it just creates the socketServer and adds a new ClientThread to a list to be handled
      */
     private void start() {
+        System.out.println(chatTime + "Server waiting for Clients on port " + port + ".");
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             while (true) {
@@ -50,6 +54,8 @@ final class ChatServer {
         int porty = Integer.parseInt(portnumb);
         ChatServer server = new ChatServer(porty);
         System.out.println(portnumb);
+        System.out.println("Banned Words:");
+
         server.start();
     }
 
@@ -73,6 +79,8 @@ final class ChatServer {
                 sOutput = new ObjectOutputStream(socket.getOutputStream());
                 sInput = new ObjectInputStream(socket.getInputStream());
                 username = (String) sInput.readObject();
+                System.out.println(chatTime + username + " just connected.");
+                System.out.println(chatTime + "Server waiting for Clients on port " + port + ".");
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
@@ -83,10 +91,8 @@ final class ChatServer {
          */
         @Override
         public void run() {
+
             while (true) {
-                if (clients.size() < 2) {
-                    System.out.println("Server waiting for Clients on port 1500.");
-                }
 
                 // Read the username sent to you by client
                 try {
@@ -123,13 +129,10 @@ final class ChatServer {
 
         private synchronized void broadcast(String message) {
             String fullMessage = "";
+            ChatFilter cf = new ChatFilter(message);
             for (int i = 0; i < clients.size(); i++) {
                 try {
-                    Date date = new Date();
-                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-                    sdf.setTimeZone(TimeZone.getTimeZone("America/Indiana/Indianapolis"));
-                    String time = sdf.format(date);
-                    fullMessage = time + " " + message;
+                    fullMessage = chatTime + " " + message;
                     clients.get(i).writeMessage(fullMessage);
                 } catch (IOException e) {
                     e.printStackTrace();
